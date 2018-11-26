e1b88330e757db82054d4623d6108e5636b842ce
Wright, Christopher R <christopher.wright@broadcom.com>
2018-11-26 13:11:57 -0500

    Document interface
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

--- src/performance/interfaces/IPerformanceTools.ts
+++ src/performance/interfaces/IPerformanceTools.ts
@@ -1,4 +1,24 @@
+/**
+ * This interface defines the methods that should exist on any {@link PerformanceTools}
+ * class. Once initially published, changes to the prototype's of these methods
+ * should not be changed so easily. Any changes to the public facing operation
+ * of these methods will result in incompatibilities with previous versions that
+ * might be loaded and called upon exit of the main program.
+ * 
+ * @see {PerfTimingClass#_savePerformanceResults}
+ */
 export interface IPerformanceTools {
+    /**
+     * Called on each {@link PerformanceTools} object found in the global symbol
+     * location. This method should return a JSON object of the metrics that
+     * the particular performance class captured.
+     */
     getMetrics(): object;
+
+    /**
+     * This method must exist on the managedApi of the main {@link PerfTimingClass}
+     * management class. It must return the values specified in the {@link INodeTiming}
+     * interface.
+     */
     getNodeTiming(): object;
 }
