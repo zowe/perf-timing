06ef5ddbe937cff4cdaa8b935480f6afeefec736
Wright, Christopher R <christopher.wright@broadcom.com>
2018-11-28 11:18:16 -0500

    Add documentation to classes
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

--- src/environment/errors/DuplicateVarError.ts
+++ src/environment/errors/DuplicateVarError.ts
@@ -1,5 +1,32 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/**
+ * This error represents the condition where an environment variable was
+ * registered twice, indicating a conflict.
+ *
+ * @example
+ * ```typescript
+ * Environment.register("key1", "val");
+ * Environment.register("key2", "val2"); // Throws error
+ * ```
+ */
 export class DuplicateVarError extends Error {
-    constructor(name: string, value: any, newValue: any) {
-        super(`"${name}" was previously registered with ${value.toString()}. Unable to re-register it with ${newValue.toString()}`);
+    /**
+     * Construct the error.
+     * @param key The environment key in conflict
+     * @param value The current default value that was set
+     * @param newValue The new default value
+     */
+    constructor(key: string, value: any, newValue: any) {
+        super(`"${key}" was previously registered with ${value.toString()}. Unable to re-register it with ${newValue.toString()}`);
     }
 }
--- src/environment/errors/NotRegisteredError.ts
+++ src/environment/errors/NotRegisteredError.ts
@@ -1,5 +1,32 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/**
+ * This error represents the condition when an environment variable was requested
+ * without first being registered.
+ *
+ * @example
+ * ```typescript
+ * Environment.register("var1", "test");
+ *
+ * Environment.get("var1"); // returns the env setting or default
+ * Environment.get("var2"); // throws error
+ * ```
+ */
 export class NotRegisteredError extends Error {
-    constructor(name: string) {
-        super(`The environment variable "${name}" has not been registered! It must be registered before the value can be retrieved.`);
+    /**
+     * Construct the error.
+     * @param key The environment key that has not been registered.
+     */
+    constructor(key: string) {
+        super(`The environment variable "${key}" has not been registered! It must be registered before the value can be retrieved.`);
     }
 }
