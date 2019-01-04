d428be11d027df921ac48f399696653b6f928d77
Wright, Christopher R <christopher.wright@broadcom.com>
2019-01-04 14:14:52 -0500

    Create test for index
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

---
+++ src/__tests__/index.unit.test.ts
@@ -0,0 +1,20 @@
+/*!
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import { PerformanceApiManager } from "../performance/manager";
+import { PerfTiming } from "../index";
+
+describe("public exports", () => {
+    it("should export a constructed api manager", () => {
+        // Simply checks that we've exported the API manager object.
+        expect(PerfTiming instanceof PerformanceApiManager).toBe(true);
+    });
+});
